0bc074b334e181311d03a89a5823bd0903dc9d86
yodev <yodev2177@gmail.com>
2020-10-14 17:58:05 +0900

    Type inference

--- packages.go
+++ packages.go
@@ -5,11 +5,6 @@ import (
 )
 
 func main() {
-	// var x, y int = 3, 4
-	// var f float64 = math.Sqrt(float64(x*x + y*y))
-	// var z uint = uint(f)
-	i := 42
-	f := float64(i)
-	u := uint(f)
-	fmt.Println(i, f, u)
+	v := 42
+	fmt.Printf("v is of type %T\n", v)
 }
