7fefbbd849f07a62b84634752e9208040386bc5d
yodev <yodev2177@gmail.com>
2020-10-14 18:02:04 +0900

    Numeric Constants

--- packages.go
+++ packages.go
@@ -4,13 +4,20 @@ import (
 	"fmt"
 )
 
-const Pi = 3.14
+const (
+	Big   = 1 << 100
+	Small = Big >> 99
+)
+
+func neetInt(x int) int {
+	return x*10 + 1
+}
+func needFloat(x float64) float64 {
+	return x * 0.1
+}
 
 func main() {
-	const World = "世界"
-	fmt.Println("Hello", World)
-	fmt.Println("Happy", Pi, "Day")
-
-	const Truth = true
-	fmt.Println("Go ules?", Truth)
+	fmt.Println(neetInt(Small))
+	fmt.Println(needFloat(Small))
+	fmt.Println(needFloat(Big))
 }
