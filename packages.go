e40a29dec054dbb33420ff7f448cd6aac91a8158
yodev <yodev2177@gmail.com>
2020-10-14 17:45:34 +0900

    split7

--- packages.go
+++ packages.go
@@ -6,10 +6,17 @@ func add(x int, y int) int {
 	return x + y
 }
 
+func split(sum int) (x, y int) {
+	x = sum * 4 / 9
+	y = sum - x
+	return
+}
+
 func main() {
 	// fmt.Println("My Favorite Number is", rand.Intn(10))
 	// fmt.Printf("Now you have %g problems .\n", math.Sqrt(7))
 	// 大文字で始まる名前は、外部のパッケージから参照できる
 	// fmt.Println(math.Pi)
-	fmt.Println(add(1, 4))
+	// fmt.Println(add(1, 4))
+	fmt.Println(split(17))
 }
