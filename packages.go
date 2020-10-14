31e02578ae86bd4980a6ff8880455eef8fd61313
yodev <yodev2177@gmail.com>
2020-10-14 17:54:14 +0900

    A Tour of Go

--- packages.go
+++ packages.go
@@ -2,9 +2,10 @@ package main
 
 import "fmt"
 
-var i, j int = 1, 2
-
 func main() {
-	var c, python, java = true, false, "no!"
-	fmt.Println(i, j, c, python, java)
+	var i int
+	var f float64
+	var b bool
+	var s string
+	fmt.Printf("%v %v %v %q\n", i, f, b, s)
 }
